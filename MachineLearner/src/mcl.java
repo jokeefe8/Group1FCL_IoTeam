e2c65202a30da0eee90e8e9560202bff5bfadaa8
jokeefe8 <jackokeefe8388@gmail.com>
2017-06-12 09:08:20 -0400

    Create mcl.java

--- MachineLearner/src/mcl.java
+++ MachineLearner/src/mcl.java
@@ -3,7 +3,7 @@ import org.jnetpcap.util.PcapPacketArrayList;
 public class mcl {
 	public static void main(String [] args) {
 		// the included pcapng file might have to be turned into a pcap file before processing. 
-		PCapHandler pcap = new PCapHandler("sample_data.pcapng");
+		PCapHandler pcap = new PCapHandler(Config.capture_file);
 		
 		try {
 			PcapPacketArrayList p_arr = pcap.readOfflineFiles();
